d8e17f6ebe01e61ed0c173dc753957844b66b70c
Diego Almeida <diegoalmda@gmail.com>
2024-03-03 21:35:47 -0300

    refactor: criar rota de referencia

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack initialRouteName="home/index">
+    <Stack initialRouteName="home/index" screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name="signIn/index"
         options={{
